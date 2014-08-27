45df6e1f20b52594cbf02033878fbe3a9bb755c2
DJAE Mamode <infomamode@yahoo.fr>
2014-08-27 21:40:10 +0400

    Create base_custom_db.class.php

---
+++ base_custom_db.class.php
@@ -0,0 +1,19 @@
+<?php
+
+abstract class Base_Custom_Db
+{
+
+    private $tableName = false;
+
+    /**
+     * Constructor for the database class to inject the table name
+     *
+     * @param String $tableName - The current table name
+     */
+     
+    public function __construct($tableName)
+    {
+        $this->tableName = $tableName;
+    }
+}
+?>
